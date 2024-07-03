86ef2cee77aaa15981e1416eb9acf0a83096bb2f
chohc51 <chohc51@naver.com>
2024-07-03 15:21:29 +0900

    2024/07/03-S3

---
+++ 1735/1735-유클리드 호제.py
@@ -0,0 +1,20 @@
+from sys import *
+import math
+
+nume1,demo1 = map(int,stdin.readline().split())
+nume2,demo2 = map(int,stdin.readline().split())
+
+demo_result = demo1*demo2
+nume_result = nume1*demo2 + demo1*nume2
+
+
+
+
+# while math.gcd(demo_result,nume_result) != 1:
+#     i = math.gcd(demo_result,nume_result)
+#     demo_result //= i
+#     nume_result //= i
+    
+            
+print(nume_result,demo_result)
+
