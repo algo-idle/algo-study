c8b5220da9b176e78a746ade3016c4ef5a90a230
MinGu Jeong <mingu830@gmail.com>
2024-07-08 22:27:09 +0900

    2024/07/08

---
+++ baekjoon/2609/2609.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a, b;
+  cin >> a >> b;
+
+  int gcd = 1;
+  for (int i = 2; i <= min(a, b); i++)
+  {
+    if (a % i == 0 && b % i == 0)
+    {
+      gcd = i;
+    }
+  }
+  cout << gcd << '\n';
+  cout << a * b / gcd << '\n';
+
+  return 0;
+}
