97449c4b7558e4f93b67ba634d79abb2af64ce0d
황보성우 <114071008+sung6770@users.noreply.github.com>
2024-08-23 14:04:38 +0900

    2024/08/23-S2

---
+++ sungwoo/baekjoon/1927/1927.py
@@ -0,0 +1,15 @@
+import sys
+read = sys.stdin.readline
+import heapq
+
+N = int(read())
+heap = []
+for _ in range(N):
+    x = int(read())
+    if x == 0:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
+    else:
+        heapq.heappush(heap, x)
